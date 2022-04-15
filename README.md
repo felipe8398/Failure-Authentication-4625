bc4e2b761cb0c5f5c924309c5c079e193571a69d
Felipe Pereira Silveira <felipe.psilveira@outlook.com>
2022-04-15 01:20:03 -0300

    Adicionando novas opções

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Failure-Authentication-4625
+# Failure-Authentication-EventID4625
+
+# OBJETIVO: #
+
+- BUSCAR LOG DE FALHA DE AUTENTICAÇÃO NO WINDOWS 
+
+# Como executar
+.\dhcp.ps1 -IP 192.168.112.200
+
+Sendo que o 192.168.112.200 é o IP que voce quer buscar, voce pode buscar por um IP, range ou pelo nome da máquina:
+
+- Buscando pelo IP 192.168.112.200
+
+.\dhcp.ps1 -IP 192.168.112.200
+
+- Buscando pela máquina USUARIO01
+
+.\dhcp.ps1 -IP USUARIO01
+
+- Buscando pelo range 192.168.112.0/24
+
+.\dhcp.ps1 -IP 192.168.112
+
+
+
+# Observação  
+Script foi testado somente no Windows Server 2012 R2 Datacenter  
+
+Ideias e melhorias são bem vindas
--- failed.ps1
+++ failed.ps1
@@ -1,10 +1,35 @@
+param ($IP,$COMPUTERNAME)
+if ($IP -like "*.*.*.*")
+{
+write-host "Realizando a busca do IP: $IP"
 Get-EventLog -LogName Security | ?{(4625) -contains $_.EventID }| %{
-    (new-object -Type PSObject -Property @{
-    TimeGenerated = $_.TimeGenerated
-    ClientIP = $_.Message -replace '(?smi).*Source Network Address:\s+([^\s]+)\s+.*','$1'
-    UserName = $_.Message -replace '(?smi).*\s\sAccount Name:\s+([^\s]+)\s+.*','$1'
-    UserDomain = $_.Message -replace '(?smi).*\s\sAccount Domain:\s+([^\s]+)\s+.*','$1'
-    LogonType = $_.Message -replace '(?smi).*Logon Type:\s+([^\s]+)\s+.*','$1'
-    WorkstationName = $_.Message -replace '(?smi).*\s\sWorkstation Name::*\s+([^\s]+)\s+.*','$1' 
-    })
-    } | sort TimeGenerated -Descending | Format-Table -Property TimeGenerated,ClientIP,UserDomain,UserName,WorkstationName,LogonType -AutoSize
+(new-object -Type PSObject -Property @{
+TimeGenerated = $_.TimeGenerated
+ClientIP = $_.Message -replace '(?smi).*Source Network Address:\s+([^\s]+)\s+.*','$1'
+UserName = $_.Message -replace '(?smi).*\s\sAccount Name:\s+([^\s]+)\s+.*','$1'
+UserDomain = $_.Message -replace '(?smi).*\s\sAccount Domain:\s+([^\s]+)\s+.*','$1'
+LogonType = $_.Message -replace '(?smi).*Logon Type:\s+([^\s]+)\s+.*','$1'
+WorkstationName = $_.Message -replace '(?smi).*\s\sWorkstation Name::*\s+([^\s]+)\s+.*','$1' 
+})
+} | sort TimeGenerated -Descending | Where-Object { $_.ClientIP -like "$IP" } | Format-Table -Property TimeGenerated,ClientIP,UserDomain,UserName,WorkstationName,LogonType -AutoSize
+}
+elseif ($COMPUTERNAME -like '*?*')
+{
+write-host "Realizando a busca pela workstation: $COMPUTERNAME"
+Get-EventLog -LogName Security | ?{(4625) -contains $_.EventID }| %{
+(new-object -Type PSObject -Property @{
+TimeGenerated = $_.TimeGenerated
+ClientIP = $_.Message -replace '(?smi).*Source Network Address:\s+([^\s]+)\s+.*','$1'
+UserName = $_.Message -replace '(?smi).*\s\sAccount Name:\s+([^\s]+)\s+.*','$1'
+UserDomain = $_.Message -replace '(?smi).*\s\sAccount Domain:\s+([^\s]+)\s+.*','$1'
+LogonType = $_.Message -replace '(?smi).*Logon Type:\s+([^\s]+)\s+.*','$1'
+WorkstationName = $_.Message -replace '(?smi).*\s\sWorkstation Name::*\s+([^\s]+)\s+.*','$1' 
+})
+} | sort TimeGenerated -Descending | Where-Object { $_.WorkstationName -like "$COMPUTERNAME" } | Format-Table -Property TimeGenerated,ClientIP,UserDomain,UserName,WorkstationName,LogonType -AutoSize
+}
+else 
+{
+write-host "VOCE PODE FAZER A BUSCA DE DUAS FORMAS"
+write-host "BUSCANDO PELO IP: .\rdp2.ps1 -IP 127.0.0.1"
+write-host "BUSCANDO PELO NOME DA MÁQUINA QUE TENTOU AUTENTICAR: .\rdp2.ps1 -COMPUTERNAME felipe-A520M-DS3H"
+}
